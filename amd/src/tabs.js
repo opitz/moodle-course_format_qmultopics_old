c242fd522929b12212bca6b09fc75f3e07769a65
Matthias Opitz <opitz@gmx.de>
2019-11-11 17:06:12 +0000

    now preserving single section title option when section is hidden

--- amd/src/tabs.js
+++ amd/src/tabs.js
@@ -233,30 +233,6 @@ define(['jquery', 'jqueryui'], function($) {
                 console.log('number of visible sections: ' + visibleSections);
                 console.log('number of hidden sections: ' + hiddenSections);
 
-                // If all visible sections are hidden for students the tab is hidden for them as well
-                // in this case mark the tab for admins so they are aware
-                if (visibleSections <= hiddenSections && visibleBlocks.length === 0 && visibleAssessmentInfo === 0) {
-                    $(this).addClass('hidden-tab');
-                    console.log("==> marking hidden tab " + tabid);
-                    var self = $(this);
-                    require(['core/str'], function(str) {
-                        var getTheString = str.get_string('hidden_tab_hint', 'format_topics2');
-                        $.when(getTheString).done(function(theString) {
-                            self.find('#not-shown-hint-' + tabid).remove();
-                            var theAppendix = '<i id="not-shown-hint-' + tabid + '" class="fa fa-info" title="' +
-                                theString + '"></i>';
-                            if ($('.tablink .fa-pencil').length > 0) { // When in edit mode ...
-                                self.find('.inplaceeditable').append(theAppendix);
-                            } else {
-                                self.append(theAppendix);
-                            }
-                        });
-                    });
-                } else {
-                    $(this).removeClass('hidden-tab');
-                    $('#not-shown-hint-' + tabid).remove();
-                }
-
                 if (visibleSections < 1 && visibleBlocks.length === 0 && visibleAssessmentInfo === 0) {
                     console.log('tab with no visible sections - hiding it');
                     $(this).parent().hide();
@@ -285,11 +261,16 @@ define(['jquery', 'jqueryui'], function($) {
                 }
 
                 // If option is set and when a tab other than tab 0 shows a single section perform some visual tricks
-                if ($('.single_section_tabs').length > 0 && tabid !== 'tab0') {
-                    var target = $('li.section:visible:not(.hidden)').first();
+                if ($('.single_section_tabs').length > 0
+                    && $(this).attr('sections').split(',').length == 1
+                    && tabid !== 'tab0') {
+                    var target = $('li.section:visible').first();
+//                    var target = $('li.section').first();
+//                    var target = $('li.section:eq(0)');
                     // If section0 is shown always on top ignore the first visible section and use the 2nd
                     if ($('.section0_ontop').length > 0) {
-                        target = $('li.section:visible:not(.hidden):eq(1)');
+//                        target = $('li.section:visible:not(.hidden):eq(1)');
+                        target = $('li.section:visible:eq(1)');
                     }
                     var firstSectionId = target.attr('id');
 
@@ -303,6 +284,34 @@ define(['jquery', 'jqueryui'], function($) {
                     } else if ($('.inplaceeditable').length > 0 && firstSectionId !== 'section-0') {
                         restoreTab($(this));
                     }
+                }
+
+                // If all visible sections are hidden for students the tab is hidden for them as well
+                // in this case mark the tab for admins so they are aware
+                if (visibleSections <= hiddenSections && visibleBlocks.length === 0 && visibleAssessmentInfo === 0) {
+                    $(this).addClass('hidden-tab');
+                    console.log("==> marking hidden tab " + tabid);
+                    var self = $(this);
+                    require(['core/str'], function(str) {
+                        var getTheString = str.get_string('hidden_tab_hint', 'format_topics2');
+                        $.when(getTheString).done(function(theString) {
+                            self.find('#not-shown-hint-' + tabid).remove();
+                            var theAppendix = '<i id="not-shown-hint-' + tabid + '" class="fa fa-info" title="' +
+                                theString + '"></i>';
+                            if (self.attr('sections').split(',').length == 1
+                                && $('.single_section_tabs').length > 0) { // if there is a single topic
+                                self.html(self.html() + ' ' +theAppendix);
+                            } else if ($('.tablink .fa-pencil').length > 0) { // When in edit mode ...
+                                self.find('.inplaceeditable').append(theAppendix);
+                            } else {
+//                                self.append(theAppendix);
+                                self.html(self.html() + ' ' +theAppendix);
+                            }
+                        });
+                    });
+                } else {
+                    $(this).removeClass('hidden-tab');
+                    $('#not-shown-hint-' + tabid).remove();
                 }
 
                 // If tab0 is alone hide it
