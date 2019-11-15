688a22662f72b6202a9e6162171a30043f365765
Matthias Opitz <opitz@gmx.de>
2019-11-15 17:27:57 +0000

    works - testing9

--- amd/src/tabs.js
+++ amd/src/tabs.js
@@ -290,7 +290,8 @@ define(['jquery', 'jqueryui'], function($) {
                 } else if (tabid === 'tab_assessment_info_block') { // Show the Assessment Info Block on the main stage
 //                    console.log('Assessment Info Block tab clicked!');
                     if ($('.block_assessment_information').length < 1) {
-                        alert('Refreshing!');
+//                        alert('Refreshing!');
+                        location.reload();
                     }
 
                     $("li.section").hide();
