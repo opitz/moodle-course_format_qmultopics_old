09db612f09c1442d178fb5508b4d21cb134fe8d8
Matthias Opitz <opitz@gmx.de>
2020-03-30 18:31:18 +0200

    removed duplicates from language file

--- lang/en/format_qmultopics.php
+++ lang/en/format_qmultopics.php
@@ -376,21 +376,6 @@ $string['single_section_tabs'] = 'Use section name as tab name for single sectio
 $string['single_section_tabs_label'] = 'Use section name as tab name for single sections';
 $string['single_section_tabs_help'] = 'When checked tabs with a single section will use the section name as tab name.';
 
-$string['tab_assessment_info_title'] = 'Assessment Information';
-$string['tab_assessment_information_title'] = 'Assessment Information';
-
-//$string['tab_assessment_info_block_title'] = 'Assessment Info Block';
-$string['tab_assessment_info_block_title'] = 'Assessment Information';
-//$string['assessment_info_block_tab'] = 'Show "Assessment Info" block under a tab';
-//$string['assessment_info_block_tab_label'] = 'Show "Assessment Info" block under a tab.';
-//$string['assessment_info_block_tab_help'] = 'When checked the "Assessment Info" block is shown under a  tab.';
-$string['assessment_info_block_tab'] = 'How to show "Assessment Info" block when installed';
-$string['assessment_info_block_tab_label'] = 'How to show the "Assessment Info" block when installed';
-$string['assessment_info_block_tab_help'] = 'When installed the "Assessment Info" block may be shown in one of 3 ways: <UL><LI>as a regular block (default)</LI><LI>under a separate tab or</LI><LI>merged with the "Assessment Information" tab.</LI></UL> <B>Please note</B> that when set to "merged" but the "Assessment Information" tab is deactivated, the "Assessment Info" Block is shown as a normal block again!';
-$string['assessment_info_block_tab_option0'] = 'Show as Block';
-$string['assessment_info_block_tab_option1'] = 'Show as Tab';
-$string['assessment_info_block_tab_option2'] = 'Merge with "Assessment Information" Tab';
-
 $string['tabtitle_edithint'] = 'Edit tab name';
 $string['tabtitle_editlabel'] = 'New value for {a}';
 
@@ -455,8 +440,10 @@ $string['single_section_tabs_help'] = 'When checked tabs with a single section w
 $string['tab_assessment_info_title'] = 'Assessment Information';
 $string['tab_assessment_information_title'] = 'Assessment Information';
 
-$string['tab_assessment_info_block_title'] = 'Assessment Info Block';
-$string['assessment_info_block_tab'] = 'How to show Assessment Info block when installed';
+//$string['tab_assessment_info_block_title'] = 'Assessment Info Block';
+$string['tab_assessment_info_block_title'] = 'Assessment Information';
+
+$string['assessment_info_block_tab'] = 'How to show "Assessment Info" block when installed';
 $string['assessment_info_block_tab_label'] = 'How to show the "Assessment Info" block when installed';
 $string['assessment_info_block_tab_help'] = 'When installed the "Assessment Info" block may be shown in one of 3 ways: <UL><LI>as a regular block (default)</LI><LI>under a separate tab or</LI><LI>merged with the "Assessment Information" tab.</LI></UL> <B>Please note</B> that when set to "merged" but the "Assessment Information" tab is deactivated, the "Assessment Info" Block is shown as a normal block again!';
 $string['assessment_info_block_tab_option0'] = 'Show as Block';
